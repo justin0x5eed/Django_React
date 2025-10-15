fff514587411e5fcca7dc33d603758da9a4c7652
Justin <justin0x5eed@gmail.com>
2025-10-15 13:23:24 +0800

    setting heroui

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { HeroUIProvider } from "@heroui/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <HeroUIProvider>{children}</HeroUIProvider>
       </body>
     </html>
   );
