651add6eced44dd53312733e17c37c28e2f17fae
justin <132219987+justin0x5eed@users.noreply.github.com>
2025-10-23 16:37:17 +0000

    Add simple Django homepage template

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from content.views import index
+
 urlpatterns = [
+    path("", index, name="index"),
     path("admin/", admin.site.urls),
     path("api/", include("content.urls")),
 ]
