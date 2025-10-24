ff4756b3285f3f210b5e0ea029c2ed6197bf270f
justin <132219987+justin0x5eed@users.noreply.github.com>
2025-10-24 03:05:42 +0000

    Fix Tailwind plugin configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,19 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      tailwindcss({
-      config: {
-        content: {
-          relative: true,
-          files: [
-            './index.html',
-            './src/**/*.{ts,tsx,js,jsx}',
-            '../backend/backend/templates/**/*.{html,txt}',
-            '../backend/backend/**/*.py',      // 如果在 Python 组件里拼接类名，也可以一并包含
-          ],
-        },
-      },
-    }),
+    react(),
+    tailwindcss(),
   ],
 })
